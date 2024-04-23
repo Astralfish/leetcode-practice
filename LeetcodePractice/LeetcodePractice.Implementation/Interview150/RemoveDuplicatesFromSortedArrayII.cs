45c812b2b52fde4b84b8c11b03403c642fd4861d
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-04-23 23:49:43 +0200

    Remove Duplicates from Sorted Array II

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+public class RemoveDuplicatesFromSortedArrayII
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums.Length;
+        }
+
+        int sameCounter = 0;
+        int compareIndex = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == nums[compareIndex])
+            {
+                nums[compareIndex + 1] = nums[i];
+                sameCounter++;
+            }
+            else
+            {
+                if (sameCounter >= 2)
+                {
+                    nums[compareIndex + 2] = nums[i];
+                    compareIndex = compareIndex + 2;
+                }
+                else
+                {
+                    nums[compareIndex + sameCounter + 1] = nums[i];
+                    compareIndex = compareIndex + sameCounter + 1;
+                }
+                sameCounter = 0;
+            }
+        }
+        if (sameCounter >= 1)
+        {
+            nums[compareIndex + 1] = nums[compareIndex];
+            compareIndex++;
+        }
+        return compareIndex + 1;
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/RemoveDuplicatesFromSoretedArrayIITests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class RemoveDuplicatesFromSoretedArrayIITests
+{
+    private readonly RemoveDuplicatesFromSortedArrayII sut;
+
+    public RemoveDuplicatesFromSoretedArrayIITests()
+    {
+        this.sut = new RemoveDuplicatesFromSortedArrayII();
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void RemoveDuplicatesShouldRemoveElementsThatOccurMoreThanTwice(int[] inputArray, int[] expected)
+    {
+        var k = sut.RemoveDuplicates(inputArray);
+        k.Should().Be(expected.Length);
+        inputArray.Take(k).Should().BeEquivalentTo(expected);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { new int[] { 1, 1, 1, 1, 2, 2, 3 }, new int[] { 1, 1, 2, 2, 3 } };
+        yield return new object[] { new int[] { 1, 1, 1, 2, 2, 2, 3, 3, 3 }, new int[] { 1, 1, 2, 2, 3, 3 } };
+    }
+}
