e17481d458ee7368b3e74101a9574307d8a4b415
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-04-26 00:00:03 +0200

    169 Majority Element

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/MajorityElement.cs
@@ -0,0 +1,5 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class MajorityElement
+{
+    public int FindMajorityElement(int[] nums) => nums.GroupBy(n => n).OrderByDescending(g => g.Count()).First().Key;
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/MajorityElementTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class MajorityElementTests
+{
+    private readonly MajorityElement sut = new MajorityElement();
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void FindMajorityElementShouldFindMajorityElement(int[] array, int expected)
+    {
+        var result = sut.FindMajorityElement(array);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { new int[] { 10 }, 10 };
+        yield return new object[] { new int[] { 1, 2, 1 }, 1 };
+        yield return new object[] { new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2 };
+    }
+}
