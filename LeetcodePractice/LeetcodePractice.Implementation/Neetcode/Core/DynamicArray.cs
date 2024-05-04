4247fd3e536131a2932d92401f5b8cb05a5139d8
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-05-04 10:23:36 +0200

    Dynamic array

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Neetcode/Core/DynamicArray.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+
+namespace LeetcodePractice.Implementation.Neetcode.Core;
+
+//https://neetcode.io/problems/dynamicArray
+public class DynamicArray : IEnumerable<int>
+{
+    private int[] data;
+    private int size;
+
+    public DynamicArray(int capacity)
+    {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    public DynamicArray(ICollection<int> initialData)
+    {
+        data = initialData.ToArray();
+        size = initialData.Count;
+    }
+
+    public int Get(int i)
+    {
+        return data[i];
+    }
+
+    public void Set(int i, int n)
+    {
+        data[i] = n;
+    }
+
+    public void PushBack(int n)
+    {
+        if (size >= data.Length)
+        {
+            Resize();
+        }
+
+        data[size++] = n;
+    }
+
+    public int PopBack()
+    {
+        if (size <= 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        return data[--size];
+    }
+
+    private void Resize()
+    {
+        var newData = new int[data.Length * 2];
+        data.CopyTo(newData, 0);
+        data = newData;
+    }
+
+    public int GetSize()
+    {
+        return size;
+    }
+
+    public int GetCapacity()
+    {
+        return data.Length;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return data.Take(size).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return data.Take(size).GetEnumerator();
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Neetcode/Core/DynamicArrayTests.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Neetcode.Core;
+
+namespace LeetcodePractice.Tests.Neetcode.Core;
+public class DynamicArrayTests
+{
+    [Theory]
+    [MemberData(nameof(GetShouldReturnValueAtIndexTestData))]
+    public void GetShouldReturnValueAtIndex(DynamicArray array, int index, int expected)
+    {
+        var result = array.Get(index);
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(SetShouldSetValueAtIndexTestData))]
+    public void SetShouldSetValueAtIndex(DynamicArray array, int index, int value, DynamicArray expected)
+    {
+        array.Set(index, value);
+        array.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(PushBackShouldAddValueToTheEndTestData))]
+    public void PushBackShouldAddValueToTheEnd(DynamicArray array, int value, DynamicArray expected)
+    {
+        array.PushBack(value);
+        array.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(PushBackShouldResizeArrayIfTooSmallTestData))]
+    public void PushBackShouldResizeArrayIfTooSmall(DynamicArray array, int value, int expectedCapacity)
+    {
+        array.PushBack(value);
+        array.GetCapacity().Should().Be(expectedCapacity);
+    }
+
+    [Theory]
+    [MemberData(nameof(PopBackShouldReturnLastElementTestData))]
+    public void PopBackShouldReturnLastElement(DynamicArray array, int expected)
+    {
+        var result = array.PopBack();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(PopBackShouldRemoveReturnedElementTestData))]
+    public void PopBackShouldRemoveReturnedElement(DynamicArray array, DynamicArray expected)
+    {
+        _ = array.PopBack();
+        array.Should().BeEquivalentTo(expected);
+    }
+
+    public static IEnumerable<object[]> GetShouldReturnValueAtIndexTestData()
+    {
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 0, -10 };
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 1, 0 };
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 2, 10 };
+    }
+
+    public static IEnumerable<object[]> SetShouldSetValueAtIndexTestData()
+    {
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 0, int.MaxValue, new DynamicArray([int.MaxValue, 0, 10]) };
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 1, -1, new DynamicArray([-10, -1, 10]) };
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 2, int.MinValue, new DynamicArray([-10, 0, int.MinValue]) };
+    }
+
+    public static IEnumerable<object[]> PushBackShouldAddValueToTheEndTestData()
+    {
+        yield return new object[] { new DynamicArray([-10, 0, 10]), int.MaxValue, new DynamicArray([-10, 0, 10, int.MaxValue]) };
+    }
+
+    public static IEnumerable<object[]> PushBackShouldResizeArrayIfTooSmallTestData()
+    {
+        yield return new object[] { new DynamicArray([-10, 0, 10]), int.MaxValue, 6 };
+    }
+
+    public static IEnumerable<object[]> PopBackShouldReturnLastElementTestData()
+    {
+        yield return new object[] { new DynamicArray([-10, 0, 10]), 10 };
+    }
+
+    public static IEnumerable<object[]> PopBackShouldRemoveReturnedElementTestData()
+    {
+        yield return new object[] { new DynamicArray([-10, 0, 10]), new DynamicArray([-10, 0]) };
+    }
+}
