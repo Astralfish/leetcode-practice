0e06e7ff7d265e2db413e9005fa99aabd724bac5
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-06-22 14:04:28 +0200

    135 Candy

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/Candy.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+
+/// <summary>
+/// https://leetcode.com/problems/candy/description/?envType=study-plan-v2&envId=top-interview-150
+/// </summary>
+public class Candy
+{
+    public int MinimumRequiredCandy(List<int> children)
+    {
+        var childrenWithCandy = children.Select(c => (rating: c, candy: 1)).ToList();
+        while (!DistributeCandy(childrenWithCandy))
+        {
+        }
+        return childrenWithCandy.Sum(x => x.candy);
+    }
+
+    public bool DistributeCandy(List<(int rating, int candy)> childrenWithCandy)
+    {
+        var wasCorrectlyDistributed = true;
+        for (int i = 0; i < childrenWithCandy.Count; i++)
+        {
+            var current = childrenWithCandy[i];
+            var leftNeighbor = i > 0 ? childrenWithCandy[i - 1] : (rating: 0, candy: 0);
+            var rightNeighbor = i < childrenWithCandy.Count - 1 ? childrenWithCandy[i + 1] : (rating: 0, candy: 0);
+            bool shouldGetAdditionalCandy = (current.rating > leftNeighbor.rating && current.candy <= leftNeighbor.candy) || (current.rating > rightNeighbor.rating && current.candy <= rightNeighbor.candy);
+            if (shouldGetAdditionalCandy)
+            {
+                wasCorrectlyDistributed = false;
+                childrenWithCandy[i] = (current.rating, current.candy + 1);
+            }
+        }
+        return wasCorrectlyDistributed;
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/CandyTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+
+public class CandyTests
+{
+    private readonly Candy sut = new Candy();
+
+    [Theory]
+    [MemberData(nameof(MinimumRequiredCandyTestData))]
+    public void MinimumRequiredCandy_ShouldReturnCorrectNumber(List<int> children, int expected)
+    {
+        var result = sut.MinimumRequiredCandy(children);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> MinimumRequiredCandyTestData()
+    {
+        yield return [new List<int> { 1, 0, 2 }, 5];
+        yield return [new List<int> { 1, 2, 2 }, 4];
+        yield return [new List<int> { 7, 7, 1, 5, 3, 4, 3, 2, 1, 2, 1 }, 20];
+    }
+}
