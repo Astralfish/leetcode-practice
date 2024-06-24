8b011ec9410ac54b190b7d3a082d5872f97ac6c8
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-06-24 22:12:38 +0200

    42 Trapping Rain Water

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/TrappingRainWater.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+
+//https://leetcode.com/problems/trapping-rain-water/description/?envType=study-plan-v2&envId=top-interview-150
+public class TrappingRainWater
+{
+    public int CalculateWaterVolume(int[] levels)
+    {
+        var leftMaxLevels = CalculateMax(levels);
+        var rightMaxLevels = CalculateMax(levels.Reverse()).Reverse();
+
+        return leftMaxLevels.Zip(rightMaxLevels, (l, r) => (l.Current, LeftMax: l.Max, RightMax: r.Max))
+            .Select(h => Math.Min(h.LeftMax, h.RightMax) - h.Current)
+            .Where(wl => wl > 0)
+            .Sum();
+    }
+
+    private IEnumerable<(int Current, int Max)> CalculateMax(IEnumerable<int> levels)
+    {
+        int currentMax = 0;
+        foreach (var level in levels)
+        {
+            currentMax = Math.Max(level, currentMax);
+            yield return (level, currentMax);
+        }
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/TrappingRainWaterTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+
+public class TrappingRainWaterTests
+{
+    public readonly TrappingRainWater sut = new TrappingRainWater();
+
+    [Theory]
+    [MemberData(nameof(TrappingRainWaterTestData))]
+    public void CalculateWaterVolume_ShouldReturnCorrectVolume(int[] levels, int expected)
+    {
+        var result = sut.CalculateWaterVolume(levels);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> TrappingRainWaterTestData()
+    {
+        yield return [new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6];
+        yield return [new int[] { 4, 2, 0, 3, 2, 5 }, 9];
+    }
+}
