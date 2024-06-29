19c43f04ef182563f5fe8ca67a56c03bc5c2e0c8
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-06-29 23:52:54 +0200

    209. Minimum Size Subarray Sum

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/MinimumSizeSubarraySum.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePractice.Implementation.Interview150;
+public class MinimumSizeSubarraySum
+{
+    public int CalculateSubarraySize(int[] numbers, int target)
+    {
+        var minimumLength = int.MaxValue;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var sum = 0;
+            var length = 0;
+            for (int j = i; j < numbers.Length; j++)
+            {
+                sum += numbers[j];
+                length++;
+
+                if (sum >= target)
+                {
+                    minimumLength = Math.Min(minimumLength, length);
+                    break;
+                }
+                if (length == numbers.Length - 1 && minimumLength == int.MaxValue)
+                {
+                    return 0;
+                }
+            }
+        }
+        return minimumLength;
+    }
+
+    public int CalculateSubarraySizeOptimized(int[] numbers, int target)
+    {
+        var windowStart = 0;
+        var windowEnd = 0;
+        var minimumLength = int.MaxValue;
+        var currentSum = 0;
+        var currentLength = 0;
+
+        while (windowEnd <= numbers.Length)
+        {
+            if (currentSum < target)
+            {
+                windowEnd++;
+                if (windowEnd > numbers.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    currentSum += numbers[windowEnd - 1];
+                    currentLength++;
+                }
+            }
+            else
+            {
+                minimumLength = Math.Min(currentLength, minimumLength);
+                windowStart++;
+                currentSum -= numbers[windowStart - 1];
+                currentLength--;
+            }
+        }
+
+        return minimumLength == int.MaxValue ? 0 : minimumLength;
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class MinimumSizeSubarraySumTests
+{
+    private readonly MinimumSizeSubarraySum sut = new MinimumSizeSubarraySum();
+
+    [Theory]
+    [MemberData(nameof(MinimumSizeSubarraySizeTestData))]
+    public void CalculateSubarraySize_ShouldReturnCorrectSubarraySize(int[] numbers, int target, int expected)
+    {
+        var result = sut.CalculateSubarraySize(numbers, target);
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(MinimumSizeSubarraySizeTestData))]
+    public void CalculateSubarraySizeOptimized_ShouldReturnCorrectSubarraySize(int[] numbers, int target, int expected)
+    {
+        var result = sut.CalculateSubarraySizeOptimized(numbers, target);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> MinimumSizeSubarraySizeTestData()
+    {
+        yield return [new int[] { 2, 3, 1, 2, 4, 3 }, 7, 2];
+        yield return [new int[] { 1, 4, 4 }, 4, 1];
+        yield return [new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 11, 0];
+    }
+}
