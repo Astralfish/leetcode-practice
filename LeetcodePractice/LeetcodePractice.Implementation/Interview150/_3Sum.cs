78e6357a11eb03ac5d59a5ce4368a57ff0f3340f
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-06-30 22:31:44 +0200

    15. 3Sum

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/_3Sum.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class _3Sum
+{
+    public IList<IList<int>> Calculate0SumTriplets(int[] nums)
+    {
+        var triplets = new List<IList<int>>();
+        nums = nums.OrderBy(n => n).ToArray();
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            var j = i + 1;
+            var k = nums.Length - 1;
+            var target = -nums[i];
+            while (j < k)
+            {
+                if (j > i + 1 && nums[j] == nums[j-1])
+                {
+                    j++;
+                    continue;
+                }
+                if (k < nums.Length - 2 && nums[k] == nums[k + 1])
+                {
+                    k--;
+                    continue;
+                }
+
+                var sum = nums[j] + nums[k];
+                if (sum < target)
+                {
+                    j++;
+                }
+                else if (sum > target)
+                {
+                    k--;
+                }
+                else
+                {
+                    triplets.Add([nums[i], nums[j], nums[k]]);
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return triplets;
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/_3SumTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class _3SumTests
+{
+    private readonly _3Sum sut = new _3Sum();
+
+    [Theory]
+    [MemberData(nameof(Calculate0SumTripletsTestData))]
+    public void Calculate0SumTriplets(int[] nums, IList<IList<int>> expected)
+    {
+        var result = sut.Calculate0SumTriplets(nums);
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    public static IEnumerable<object[]> Calculate0SumTripletsTestData()
+    {
+        yield return [new int[] { -1, 0, 1, 2, -1, -4 }, new List<IList<int>>
+        {
+            new List<int> { -1,-1,2 },
+            new List<int> { -1, 0, 1 },
+        }];
+    }
+}
