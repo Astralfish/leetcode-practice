164ecb84dcf3c18e12c98c898faedbb5f3aa83ae
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-02 22:38:38 +0200

    25. Reverse Nodes in k-Group

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/ReverseNodesInKGroup.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class ReverseNodesInKGroup
+{
+    public ListNode ReverseKGroup(ListNode head, int k)
+    {
+        var preHead = new ListNode(0, head);
+        var kGroupPreHead = preHead;
+
+        var current = head;
+        var stack = new Stack<ListNode>(k);
+        while (current != null)
+        {
+            stack.Push(current);
+            if (stack.Count == k)
+            {
+                var currentKGroup = kGroupPreHead;
+                var next = current.next;
+                while (stack.Count > 0)
+                {
+                    currentKGroup.next = stack.Pop();
+                    currentKGroup = currentKGroup.next;
+                }
+                currentKGroup.next = next;
+                kGroupPreHead = currentKGroup;
+                current = kGroupPreHead;
+            }
+            current = current.next;
+        }
+        return preHead.next;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/ReverseNodesInKGroupTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+using static LeetcodePractice.Implementation.Interview150.ReverseNodesInKGroup;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class ReverseNodesInKGroupTests
+{
+    private readonly ReverseNodesInKGroup sut = new ReverseNodesInKGroup();
+
+    [Theory]
+    [MemberData(nameof(ReverseNodesInKGroupTestData))]
+    public void ReverseNodesInKGroup_ShouldReverseNodesKAtATime(ListNode head, int k, ListNode expected)
+    {
+        var result = sut.ReverseKGroup(head, k);
+
+        var currentResultNode = result;
+        var currentExpectedNode = expected;
+
+        while (currentExpectedNode != null)
+        {
+            currentResultNode.Should().NotBeNull();
+            currentResultNode!.val.Should().Be(currentExpectedNode.val);
+
+            currentResultNode = currentResultNode.next;
+            currentExpectedNode = currentExpectedNode.next;
+        }
+        currentResultNode.Should().BeNull();
+    }
+
+    public static IEnumerable<object[]> ReverseNodesInKGroupTestData()
+    {
+        yield return [ArrayToLinkedList([1, 2, 3, 4, 5]), 2, ArrayToLinkedList([2, 1, 4, 3, 5])];
+        yield return [ArrayToLinkedList([1, 2, 3, 4, 5]), 3, ArrayToLinkedList([3, 2, 1, 4, 5])];
+        yield return [ArrayToLinkedList([1, 2, 3, 4, 5]), 5, ArrayToLinkedList([5, 4, 3, 2, 1])];
+    }
+
+    private static ListNode ArrayToLinkedList(int[] array)
+    {
+        ListNode preHead = new ListNode();
+        ListNode current = preHead;
+        foreach (int i in array)
+        {
+            current.next = new ListNode(i);
+            current = current.next;
+        }
+        return preHead.next!;
+    }
+}
