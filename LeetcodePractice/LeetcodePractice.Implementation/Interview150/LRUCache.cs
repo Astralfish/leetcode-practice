71c0ee1c9855a8661eae361abd6f6c164e65b193
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-03 21:23:06 +0200

    146. LRU Cache

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/LRUCache.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class LRUCache
+{
+    private LinkedList<(int key, int value)> lastAccessTracker = new LinkedList<(int, int)>();
+    private Dictionary<int, LinkedListNode<(int key, int value)>> cache = new Dictionary<int, LinkedListNode<(int, int)>>();
+    private int capacity;
+
+    public LRUCache(int capacity)
+    {
+        this.capacity = capacity;
+    }
+
+    public int Get(int key)
+    {
+        if (cache.TryGetValue(key, out var node))
+        {
+            RefreshNode(node);
+            return node.Value.value;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public void Put(int key, int value)
+    {
+        if (cache.TryGetValue(key, out var node))
+        {
+            RefreshNode(node);
+            node.Value = (key, value);
+        }
+        else
+        {
+            var newNode = lastAccessTracker.AddFirst((key, value));
+            cache.Add(key, newNode);
+
+            if (cache.Count > capacity)
+            {
+                var lruNode = lastAccessTracker.Last;
+                lastAccessTracker.RemoveLast();
+                cache.Remove(lruNode!.Value.key);
+            }
+        }
+    }
+
+    private void RefreshNode(LinkedListNode<(int, int)> node)
+    {
+        lastAccessTracker.Remove(node);
+        lastAccessTracker.AddFirst(node);
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/LRUCacheTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class LRUCacheTests
+{
+    private LRUCache sut;
+
+    [Fact]
+    public void LRUCache_ShouldRetrieveCorrectItems()
+    {
+        sut = new LRUCache(2);
+        using (new AssertionScope())
+        {
+            sut.Put(1, 1);
+            sut.Put(2, 2);
+            sut.Get(1).Should().Be(1);
+            sut.Put(3, 3);
+            sut.Get(2).Should().Be(-1);
+            sut.Put(4, 4);
+            sut.Get(1).Should().Be(-1);
+            sut.Get(3).Should().Be(3);
+            sut.Get(4).Should().Be(4);
+        }
+    }
+}
