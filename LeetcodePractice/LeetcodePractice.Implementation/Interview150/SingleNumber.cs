8ef6114427823017c6e8f47eebea16646d9c00a0
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-05 23:41:36 +0200

    136. Single Number

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/SingleNumber.cs
@@ -0,0 +1,8 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class SingleNumber
+{
+    public int FindSingleNumber(int[] numbers)
+    {
+        return numbers.Aggregate((acc, curr) => acc ^ curr);
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/SingleNumberTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class SingleNumberTests
+{
+    private readonly SingleNumber sut = new SingleNumber();
+
+    [Theory]
+    [MemberData(nameof(FindSingleNumberTestData))]
+    public void FindSingleNumber_ShouldReturnSingleNumber(int[] numbers, int expected)
+    {
+        var result = sut.FindSingleNumber(numbers);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> FindSingleNumberTestData()
+    {
+        yield return [new int[] { 2, 2, 1 }, 1];
+        yield return [new int[] { 4, 1, 2, 1, 2 }, 4];
+        yield return [new int[] { 1 }, 1];
+    }
+}
