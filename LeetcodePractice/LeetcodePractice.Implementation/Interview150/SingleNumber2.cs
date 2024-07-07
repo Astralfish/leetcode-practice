2a556b1dc528d4026d4372f82ce603b2fcc75018
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-07 22:04:31 +0200

    137. Single Number II

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/SingleNumber2.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class SingleNumber2
+{
+    public int FindSingleNumber(int[] numbers) => numbers.Aggregate((r1: 0, r2: 0), BitwiseModulo3).r1;
+
+    public (int r1, int r2) BitwiseModulo3((int r1, int r2) left, int right)
+    {
+        return ((left.r1 & ~right) | (~left.r1 & ~left.r2 & right), (left.r1 & right) | (left.r2 & ~right));
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/SingleNumber2Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class SingleNumberTests2
+{
+    private readonly SingleNumber2 sut = new SingleNumber2();
+
+    [Theory]
+    [MemberData(nameof(FindSingleNumberTestData))]
+    public void FindSingleNumber_ShouldReturnSingleNumber(int[] numbers, int expected)
+    {
+        var result = sut.FindSingleNumber(numbers);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> FindSingleNumberTestData()
+    {
+        yield return [new int[] { 2, 2, 3, 2 }, 3];
+        yield return [new int[] { 0, 1, 0, 1, 0, 1, 99 }, 99];
+        yield return [new int[] { 1 }, 1];
+    }
+}
