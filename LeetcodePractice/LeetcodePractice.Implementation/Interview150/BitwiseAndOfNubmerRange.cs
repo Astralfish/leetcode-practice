0f3ff400ddc591f277a132973bfba45ba3f782a2
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-08 21:05:18 +0200

    201. Bitwise AND of Numbers Range

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/BitwiseAndOfNubmerRange.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace LeetcodePractice.Implementation.Interview150;
+public class BitwiseAndOfNumberRange
+{
+    public int BitwiseAnd(int from, int to)
+    {
+        if (from == to)
+        {
+            return from;
+        }
+
+        var xor = from ^ to;
+        var sameLeadingBitsCount = BitOperations.LeadingZeroCount((uint)xor);
+        var mask = ~(0xffffffff >>> sameLeadingBitsCount);
+        return (int)(from & mask);
+    }
+
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/BitwiseAndOfNumberRangeTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class BitwiseAndOfNumberRangeTests
+{
+    private readonly BitwiseAndOfNumberRange sut = new BitwiseAndOfNumberRange();
+
+    [Theory]
+    [MemberData(nameof(BitwiseAndTestData))]
+    public void BitwiseAnd_ShouldReturnAndOfAllNumbersBetweenFromAndTo(int from, int to, int expected)
+    {
+        var result = sut.BitwiseAnd(from, to);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> BitwiseAndTestData()
+    {
+        yield return [5, 7, 4];
+        yield return [0, 0, 0];
+        yield return [1, 2147483647, 0];
+        yield return [1, 1, 1];
+    }
+}
