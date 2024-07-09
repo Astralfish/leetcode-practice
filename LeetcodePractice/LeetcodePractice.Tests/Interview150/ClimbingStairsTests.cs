6438e40a33317d7874746be9edb9f17c7f9ca019
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-09 21:08:21 +0200

    70. Climbing Stairs

---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/ClimbingStairsTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class ClimbingStairsTests
+{
+    private readonly ClimbingStairs sut = new();
+
+    [Theory]
+    [MemberData(nameof(HowManyWaysTestData))]
+    public void HowManyWays_ShouldReturnNumberOfWaysToClimbNStairs(int n, int expected)
+    {
+        var result = sut.HowManyWays(n);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> HowManyWaysTestData()
+    {
+        yield return [2, 2];
+        yield return [3, 3];
+        yield return [4, 5];
+    }
+}
