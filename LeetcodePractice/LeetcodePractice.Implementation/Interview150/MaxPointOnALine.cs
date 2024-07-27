ec3a6d48e658facdf6e6d7f7fa975f80029fe556
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-27 21:51:02 +0200

    149. Max Points on a Line

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/MaxPointOnALine.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+public class MaxPointOnALine
+{
+    public int GetMax(int[][] points)
+    {
+        if (points.Length <= 2)
+        {
+            return points.Length;
+        }
+
+        var max = 2;
+        for (int i = 0; i < points.Length; i++)
+        {
+            for (int j = i + 1; j < points.Length; j++)
+            {
+                var currentCount = 2;
+                for (int k = 0; k < points.Length; k++)
+                {
+                    if (k != i && k != j && AreColinear(points[i], points[j], points[k]))
+                    {
+                        currentCount++;
+                    }
+                }
+
+                max = Math.Max(max, currentCount);
+            }
+        }
+
+        return max;
+    }
+
+    private static bool AreColinear(int[] a, int[] b, int[] c) => (c[1] - a[1]) * (b[0] - a[0]) == (b[1] - a[1]) * (c[0] - a[0]);
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/MaxPointOnALineTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class MaxPointOnALineTests
+{
+    private readonly MaxPointOnALine sut = new MaxPointOnALine();
+
+    [Theory]
+    [MemberData(nameof(GetMaxTestData))]
+    public void GetMax_ShouldReturnMaximumNumberOFColinearPoints(int[][] points, int expected)
+    {
+        var result = sut.GetMax(points);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> GetMaxTestData()
+    {
+        yield return [new int[][] { [1, 1], [2, 2], [3, 3] }, 3];
+        yield return [new int[][] { [2, 3], [3, 3], [-5, 3] }, 3];
+        yield return [new int[][] { [4, 5], [4, -1], [4, 0] }, 3];
+        yield return [new int[][] { [-6, -1], [3, 1], [12, 3] }, 3];
+        yield return [new int[][] { [1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4] }, 4];
+        yield return [new int[][] { [1, 1], [3, 2],  }, 2];
+        yield return [new int[][] { [1, 1] }, 1];
+        yield return [new int[][] { }, 0];
+    }
+}
