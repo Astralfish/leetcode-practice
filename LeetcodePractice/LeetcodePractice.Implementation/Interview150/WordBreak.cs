a11b568f80c15fbfee3ac356d0257287879dd9be
Astralfish <32631648+Astralfish@users.noreply.github.com>
2024-07-31 23:23:50 +0200

    139. Word Break

---
+++ LeetcodePractice/LeetcodePractice.Implementation/Interview150/WordBreak.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetcodePractice.Implementation.Interview150;
+
+public class WordBreak
+{
+    public bool CanBeBroken(string word, IList<string> fragments)
+    {
+        var breakpoints = new List<bool> { true }.Concat(Enumerable.Repeat(false, word.Length)).ToList();
+
+        for (int i = 1; i <= word.Length; i++)
+        {
+            foreach (var fragment in fragments)
+            {
+                var start = i - fragment.Length;
+                if (start >= 0 && breakpoints[start] && word.Substring(start).StartsWith(fragment))
+                {
+                    breakpoints[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return breakpoints.Last();
+    }
+}
---
+++ LeetcodePractice/LeetcodePractice.Tests/Interview150/WordBreakTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetcodePractice.Implementation.Interview150;
+
+namespace LeetcodePractice.Tests.Interview150;
+public class WordBreakTests
+{
+    private readonly WordBreak sut = new WordBreak();
+
+    [Theory]
+    [MemberData(nameof(CanBeBrokenTestData))]
+    public void CanBeBroken_ShouldReturnWhetherWordCanBeBrokenIntoFragments(string word, List<string> fragments, bool expected)
+    {
+        var result = sut.CanBeBroken(word, fragments);
+        result.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> CanBeBrokenTestData()
+    {
+        yield return ["applepenapple", new List<string> { "apple", "pen" }, true];
+        yield return ["catsandog", new List<string> { "cats","dog","sand","and","cat" }, false];
+        yield return ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", new List<string> { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa" }, false];
+    }
+}
